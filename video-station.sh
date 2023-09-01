6f2df3dc88d7f4c05fceef02077efb2ea0b18883
poilobo <poilobo@users.noreply.github.com>
2023-08-31 19:01:40 -0700

    Update video-station.sh

--- video-station.sh
+++ video-station.sh
@@ -8,8 +8,17 @@ sudo apt -y install ansible git python3-pip python3-dev python3-rpi.gpio
 # Create installation directory
 mkdir -p $CONFIG_DIR
 
-# Clone in the pather-creek station
-git clone $GIT_REPO $CONFIG_DIR
+# Change into the target
+cd $CONFIG_DIR
+
+# Clone or update pather-creek-station repo
+if [! -d $CONFIG_DIR ]; then
+  git clone $GIT_REPO
+else
+  git pull 
+fi
+
+
 
 
 
