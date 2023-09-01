24e4bdd534dbd1c5fbe04283383ec149000345a1
poilobo <poilobo@users.noreply.github.com>
2023-08-31 18:57:25 -0700

    Update video-station.sh

--- video-station.sh
+++ video-station.sh
@@ -3,12 +3,14 @@ CONFIG_DIR=/opt/panther-creek-station
 GIT_REPO=https://github.com/FromTheCabin/panther-creek-station.git
 
 # Make sure ansible and git are present
-sudo apt -y install ansible git
+sudo apt -y install ansible git python3-pip python3-dev python3-rpi.gpio
 
 # Create installation directory
 mkdir -p $CONFIG_DIR
 
 # Clone in the pather-creek station
 git clone $GIT_REPO $CONFIG_DIR
+
+
 
 
